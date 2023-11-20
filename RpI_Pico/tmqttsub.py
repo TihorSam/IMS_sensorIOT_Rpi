a0ec6953ba6825b394b3c5b1871be37853bc3471
TihorSam <108570778+TihorSam@users.noreply.github.com>
2023-11-20 17:40:30 +0530

    Update tmqttsub.py

--- RpI_Pico/tmqttsub.py
+++ RpI_Pico/tmqttsub.py
@@ -1,8 +1,4 @@
-import paho.mqtt.client as paho
-#pip3 install paho-mqtt
-global mqttclient;
-global broker;
-global port;
+import paho.mqtt.client as mqtt
 
 # Define MQTT broker information
 MQTT_BROKER_HOST = "192.168.1.109"
@@ -11,14 +7,13 @@ MQTT_TOPIC = "iot/picow"
 
 # MQTT callback when the client connects
 def on_connect(client, userdata, flags, rc):
-	print("Connected to MQTT broker with result code " + str(rc))
-	# Subscribe to the MQTT topic
-	client.subscribe(MQTT_TOPIC)
+    print("Connected to MQTT broker with result code " + str(rc))
+    # Subscribe to the MQTT topic
+    client.subscribe(MQTT_TOPIC)
 
-def _on_message(client, userdata, msg):
-	# print("Received: Topic: %s Body: %s", msg.topic, msg.payload)
-	print(msg.topic+" "+str(msg.payload))
-
+# MQTT callback when a message is received
+def on_message(client, userdata, msg):
+    print(msg.topic + " " + str(msg.payload))
 
 # Initialize MQTT client
 client = mqtt.Client()
