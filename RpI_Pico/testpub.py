405675179426f9fb15c3501db0852b508f3779c3
TihorSam <108570778+TihorSam@users.noreply.github.com>
2023-11-24 12:59:11 +0530

    Update testpub.py

--- RpI_Pico/testpub.py
+++ RpI_Pico/testpub.py
@@ -4,8 +4,8 @@ import json
 MQTT_BROKER_HOST = '192.168.1.109'
 MQTT_BROKER_PORT = 1883
 MQTT_TOPIC = "iot/picow"
-# data = input("ENTER THE COMMAND")
-data = 'hello PicoW'
+data = input("ENTER THE COMMAND: ")
+# data = 'hello PicoW'
 
 client = mqtt.Client()
 client.connect(MQTT_BROKER_HOST, MQTT_BROKER_PORT, 60)
